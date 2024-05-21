08f687fc42efb91aa6413f6149ef971d94a05c42
smavend <amanrique068@gmail.com>
2024-05-21 01:42:01 -0500

    build projec

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,23 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://smavend.github.io";
+//const SCRIPT = includes('build');
+const isBuild = process.argv.includes("build");
+
+let BASE_URL = LOCALHOST_URL;
+
+if  (isBuild) {
+  BASE_URL = LIVE_URL
+}
+
 // https://astro.build/config
 export default defineConfig({
+  server: { port: SERVER_PORT },
+  site: BASE_URL,
   integrations: [tailwind()],
-
-  site: 'https://smavend.github.io',
   output: 'static',
   outDir: './docs',
 
